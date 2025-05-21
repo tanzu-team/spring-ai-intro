52709b26f14925cffefde85189ac12aa8d18390e
Timo Salm <accounts@timosalm.de>
2025-05-21 23:47:26 +0200

    Spring AI 1.0 update

--- src/main/java/dev/tanzu/demo/config/SimpleVectorStoreConfig.java
+++ src/main/java/dev/tanzu/demo/config/SimpleVectorStoreConfig.java
@@ -3,18 +3,10 @@ package dev.tanzu.demo.config;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableAutoConfiguration(
-    exclude = {
-        RedisAutoConfiguration.class
-    }
-)
 class SimpleVectorStoreConfig {
 
     @Bean
